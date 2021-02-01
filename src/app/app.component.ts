8b01c4954ba61a7b6314d48dca8e23b0b54b1428
David Alberici <davidalberici3200@gmail.com>
2021-02-01 13:04:42 +0100

    Added check to set default language equal to browser language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,10 @@ export class AppComponent {
   title = 'ngx-translate-test1'
 
   constructor(private translate: TranslateService) {
-    translate.setDefaultLang('en')
+    if (window.navigator.languages && window.navigator.languages[0] && window.navigator.languages[0].includes("de"))
+      translate.setDefaultLang('de')
+    else
+      translate.setDefaultLang('en')
   }
 
   useLanguage(language: string) {
